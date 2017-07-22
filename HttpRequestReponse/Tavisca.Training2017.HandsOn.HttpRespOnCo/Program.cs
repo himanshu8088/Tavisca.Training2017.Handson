c3b709a0e608c7dbbc6106e6252ea4ef9cc0d637
Himanshu Soni <hsoni@tavisca.com>
2017-07-22 14:21:39 +0530

    Http Request Project refractored

--- HttpRequestReponse/Tavisca.Training2017.HandsOn.HttpRespOnCo/Program.cs
+++ HttpRequestReponse/Tavisca.Training2017.HandsOn.HttpRespOnCo/Program.cs
@@ -13,35 +13,14 @@ namespace Tavisca.Training2017.HandsOn.HttpRespOnCo
         {
             HttpWebRequest request = HttpWebRequest.CreateHttp("http://codejam.tavisca.com/");
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-            // Get the stream associated with the response.
+          
             Stream receiveStream = response.GetResponseStream();
-
-            // Pipes the stream to a higher level stream reader with the required encoding format. 
-            StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
-
-            /*
-            using (Stream file = File.Create("C:\\Users\\hsoni\\Desktop\\f.html"))
-            {
-                CopyStream(receiveStream, file);
-            }
-            */
+            StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);            
             Console.WriteLine("Response stream received.\n");
             Console.WriteLine(readStream.ReadToEnd());
             response.Close();
             readStream.Close();
             Console.ReadKey();
         }
-        /*
-        public static void CopyStream(Stream input, Stream output)
-        {
-            byte[] buffer = new byte[8 * 1024];
-            int len;
-            while ((len = input.Read(buffer, 0, buffer.Length)) > 0)
-            {
-                output.Write(buffer, 0, len);
-            }
-        }
-        */
     }
 }
